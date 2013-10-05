c8c83774ee0a7b4fbd782257a56c293f5806fb2b
Brian Ridings <bdrid001@gmail.com>
2013-10-05 04:50:06 -0400

    Slimed down app.js added socket.io server

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 var express = require('express')
-	, routes  = require('./routes/')
-	, config  = require('./configure/config')
+	, routes  = require('./routes/routes')
+	, config  = require('./config')
 	, https   = require('https')
 	, path    = require('path')
 	, io	    = require('socket.io');
@@ -11,23 +11,26 @@ var schema = require('./models/schema');
 var app = express();
 
 // all environments
-app.set('port', config.https_port);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.configure(function(){
+	app.set('port', config.https_port);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(path.join(__dirname, 'public')));
+});
 
 // development only
 //if ('development' == app.get('env')) {
 app.use(express.errorHandler());
 //}
-app.get('/', routes.index);
-app.get('/chat', routes.chat);
+routes.route(app);
 
 var server = https.createServer(config.certs, app).listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
 });
+
+var socket = io.listen(server);
