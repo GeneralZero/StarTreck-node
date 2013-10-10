fca0dc69cf18e4ab2407f16ef86d4d6cdcc477ca
Brian Ridings <bdrid001@gmail.com>
2013-10-09 21:53:38 -0400

    Added user to Routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,17 @@
 var index = require("./index").setVarables({title: "Express"});
 var chat = require("./chat").setVarables({title: "Express"});
+var login = require("./login").setVarables({title: "Express"});
+var passport = require('passport');
 
 exports.route = function(app){
-    app.get('/', index);
-    app.get('/chat', chat);
+	app.get('/', index);
+	app.get('/chat', chat);
+	app.get('/login', login);
+
+    app.post('/login', passport.authenticate('local'),
+		function(req, res) {
+			// If this function gets called, authentication was successful.
+			// `req.user` contains the authenticated user.
+			res.redirect('/users/' + req.user.username);
+		});
 };
