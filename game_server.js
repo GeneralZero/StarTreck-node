041f62027b9df5ed7eed80a79f69599e58acf2ba
Brian Ridings <bdrid001@gmail.com>
2014-05-13 17:06:21 -0400

    Updating aplication

---
+++ game_server.js
@@ -0,0 +1,35 @@
+//Game Database Dependencies
+var gameSchema = require('./models/game');
+var playerSchema = require('./models/player');
+var UserSchema = require('./models/User');
+
+
+function loadRequestedData (user) {
+	//Verify User's Cookie
+
+	//getVisableSquares()
+
+	//GetData on visable squares
+}
+
+function endOfTurn(user, data){
+	//Verify User's Cookie
+
+	//Check Data To see if correct
+
+	//Add Data to database
+
+	//Signal Next Turn
+}
+
+exports.init = function(io, sessionSockets){
+	sessionSockets.on('connection', function (err, socket, session) {
+		socket.emit('session', session);
+
+		socket.on('foo', function(value) {
+			session.foo = value;
+			session.save();
+			socket.emit('session', session);
+		});
+	});
+}
