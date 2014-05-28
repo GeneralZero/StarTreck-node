2a3b5196c826cff04f628a80f8fb013ac9c01b68
Brian Ridings <bdrid001@gmail.com>
2014-05-28 17:18:34 -0400

    Update board_square.js
    Add Viewable object to database to easily find those who can view this square.

--- models/board_square.js
+++ models/board_square.js
@@ -2,12 +2,14 @@ var mongoose = require('mongoose');
 
 //Connected Attributies
 var planetSchema = require('./planet');
+var playerSchema = require('./player');
 
 var board_squareSchema = new mongoose.Schema({
 	title:     { type: String},
 	content:   { type: String},
 	published: { type: Boolean, default: false, index: true },
 	is_free:   { type: Boolean, default: true },
+	viewable: [mongoose.Schema.ObjectId], //Players that can view this square
 	planet: [planetSchema]
 });
 
